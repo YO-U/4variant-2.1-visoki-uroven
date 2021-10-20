fc569d098c98bf3dc2dc9da776efd03914241898
enoxy <enoxy@DESKTOP-8BHQKRR>
2021-10-20 16:00:23 +0200

    Добавьте файлы проекта.

---
+++ 4variant 2.1 visoki uroven/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _4variant_2._1_visoki_uroven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            { 
+                Console.WriteLine("Введите х");
+                double x = double.Parse(Console.ReadLine());
+                Console.WriteLine("Введите y");
+                double y = double.Parse(Console.ReadLine());
+                if (x >= 0 && x <= 1 && y <= 1 && y >= -1) Console.WriteLine("Истина");
+                else Console.WriteLine("Ложно");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
